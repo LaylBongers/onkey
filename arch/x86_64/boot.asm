599920878fc6f75f2e5e9ba0726ce33c3aba8e3e
Layl <LaylConway@users.noreply.github.com>
2016-05-24 00:07:00 +0200

    Make asm_print callable with parameters

--- arch/x86_64/boot.asm
+++ arch/x86_64/boot.asm
@@ -67,22 +67,56 @@ start:
 bits 64
 
 long_mode_start:
+    ; Set the stack pointer
+    mov rsp, stack_end
+
+    ; Print a hello world
+    push 2
+    push hello_world_msg
     call asm_print
+    add esp, 16
+
+    ; Print a hello again
+    push 82
+    push hello_again_msg
+    call asm_print
+    add esp, 16
+
     hlt
 
+guard:
+    jmp guard
+
 asm_print:
-    mov ecx, 0              ; null the loop counter
+    ; Callee responsibilities
+    push rbp        ; store caller's stack frame
+    mov rbp, rsp    ; make current stack frame the stack pointer
+
+    ; Get the position on screen we need to print at and multiply it by 2
+    mov rbx, [rbp+24]
+    mov rax, 2
+    mul rbx
+    mov rbx, rax
+
+    ; Get the text we need to print
+    mov rdx, [rbp+16]
+
+    ; Perform the actual print loop
+    mov rcx, 0 ; null the loop counter
     .asm_print_loop:
         ; Write the character to the target location
         mov ah, 0x02
-        mov al, [hello_world + ecx]
-        mov word [0xb8000 + ecx * 2], ax
+        mov al, [rdx + rcx]
+        mov word [0xb8000 + rbx + rcx*2], ax
 
         ; Loop check
         inc ecx                             ; Increment the loop counter
-        cmp byte [hello_world + ecx], 0x0   ; Check if the new character is null
+        cmp byte [rdx + rcx], 0x0   ; Check if the new character is null
         jne .asm_print_loop                 ; If it isn't, continue
 
+    ; Callee responsibilities
+    mov rsp, rbp    ; Remove all local pushes
+    pop rbp         ; Restore caller's stack frame
     ret
 
 
@@ -96,15 +130,19 @@ p3_table:
 p2_table:
     resb 4096
 
+stack_begin:
+    resb 4096   ; Reserve 4 KiB stack space
+stack_end:
 
 section .rodata
 
 gdt64: ; The full (read-only) GDT
-    dq 0 ; It's a zero entry
+    dq 0 ; We aren't actually using the GDT so, zero entry
     .code: equ $ - gdt64
         dq (1<<44) | (1<<47) | (1<<41) | (1<<43) | (1<<53) ; Code segment
     .data: equ $ - gdt64
         dq (1<<44) | (1<<47) | (1<<41) ; Data segment
+
         ; In the code and data segments:
         ; 44: ‘descriptor type’: This has to be 1 for code and data segments
         ; 47: ‘present’: This is set to 1 if the entry is valid
@@ -116,8 +154,11 @@ gdt64: ; The full (read-only) GDT
         dw .pointer - gdt64 - 1 ; Length
         dq gdt64                ; Address
 
-
-section .data
-
-hello_world:
+hello_world_msg:
     db "Hello, World!",0
+
+hello_again_msg:
+    db "Hello, Again!",0
+
+guard_msg:
+    db "Leaked into the guard!",0
