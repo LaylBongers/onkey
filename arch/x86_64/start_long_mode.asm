8dff202305bcba84736f9ea1de417574f8192ee8
Layl <LaylConway@users.noreply.github.com>
2016-05-26 00:53:25 +0200

    Use r# registers

--- arch/x86_64/start_long_mode.asm
+++ arch/x86_64/start_long_mode.asm
@@ -1,3 +1,5 @@
+%use altreg
+
 global start_long_mode
 
 extern stack_end
@@ -15,21 +17,21 @@ start_long_mode:
     push 2
     push hello_world_msg
     call asm_print
-    add esp, 16
+    add rsp, 16
 
     ; Print a hello again
     push 82
     push hello_again_msg
     call asm_print
-    add esp, 16
+    add rsp, 16
 
     ; Show that the kernel has returned, this should be unreachable
-    mov rax, 0x4f724f204f534f4f
-    mov [0xb8000], rax
-    mov rax, 0x4f724f754f744f65
-    mov [0xb8008], rax
-    mov rax, 0x4f214f644f654f6e
-    mov [0xb8010], rax
+    mov r0, 0x4f724f204f534f4f
+    mov [0xb8000], r0
+    mov r0, 0x4f724f754f744f65
+    mov [0xb8008], r0
+    mov r0, 0x4f214f644f654f6e
+    mov [0xb8010], r0
 
     hlt
 
@@ -39,32 +41,31 @@ asm_print:
     mov rbp, rsp    ; Make the new stack frame equal the current stack pointer
 
     ; Get the position on screen we need to print at and multiply it by 2
-    mov rbx, [rbp+24]
-    mov rax, 2
-    mul rbx
-    mov rbx, rax
+    mov r1, [rbp+24]
+    mov r0, 2
+    mul r1
+    mov r1, r0
 
     ; Get the text we need to print
-    mov rdx, [rbp+16]
+    mov r3, [rbp+16]
 
     ; Perform the actual print loop
-    mov rcx, 0 ; null the loop counter
+    mov r2, 0 ; null the loop counter
     .asm_print_loop:
         ; Write the character to the target location
         mov ah, 0x02
-        mov al, [rdx + rcx]
-        mov word [0xb8000 + rbx + rcx*2], ax
+        mov al, [r3 + r2]
+        mov word [0xb8000 + r1 + r2*2], ax
 
         ; Loop check
-        inc rcx                     ; Increment the loop counter
-        cmp byte [rdx + rcx], 0x0   ; Check if the new character is null
+        inc r2                     ; Increment the loop counter
+        cmp byte [r3 + r2], 0x0   ; Check if the new character is null
         jne .asm_print_loop         ; If it isn't, continue
 
     ; Callee responsibilities
     mov rsp, rbp    ; Remove all local pushes
     pop rbp         ; Restore caller's stack frame
     ret
-
 
 section .rodata
 
